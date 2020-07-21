81e1acca1fc7425b119e837a88d34fc661aa0a79
David Horstman <davidzilla27@gmail.com>
2020-07-21 12:48:07 -0400

    Agnostic binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,5 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
-    if not start:
-        start = 0
+def binary_search(arr, target, start=0, end=None):
     if not end:
         end = len(arr)-1
     if end < start:
@@ -21,5 +19,22 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
-    pass
+def agnostic_binary_search(arr, target, start=0, end=None):
+    if not end:
+        end = len(arr)-1
+    if end < start:
+        return -1
+    ascending = arr[0] < arr[-1]
+    mid = (start+end)//2
+    if arr[mid] == target:
+        return mid
+    if ascending:
+        if arr[mid] < target:
+            return agnostic_binary_search(arr, target, mid+1, end)
+        else:
+            return agnostic_binary_search(arr, target, start, mid-1)
+    else:
+        if arr[mid] < target:
+            return agnostic_binary_search(arr, target, start, mid-1)
+        else:
+            return agnostic_binary_search(arr, target, mid+1, end)
