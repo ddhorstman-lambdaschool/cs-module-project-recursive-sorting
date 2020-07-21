66fc71eedf37ebc1e23a0fc256dc8b288283ff18
David Horstman <davidzilla27@gmail.com>
2020-07-21 14:03:13 -0400

    Start in place merge. Speed up regular merge

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,17 +1,22 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
-    for i in range(elements):
-        if len(arrA) > 0 and len(arrB) > 0:
-            if arrA[0] < arrB[0]:
-                merged_arr[i] = arrA.pop(0)
+    lenA, lenB = len(arrA), len(arrB)
+    merged_arr = [0] * (lenA+lenB)
+    a = b = 0
+    for i in range(lenA+lenB):
+        if lenA > a and lenB > b:
+            if arrA[a] < arrB[b]:
+                merged_arr[i] = arrA[a]
+                a += 1
             else:
-                merged_arr[i] = arrB.pop(0)
-        elif len(arrA) > 0:
-            merged_arr[i] = arrA.pop(0)
+                merged_arr[i] = arrB[b]
+                b += 1
+        elif lenA > a:
+            merged_arr[i] = arrA[a]
+            a += 1
         else:
-            merged_arr[i] = arrB.pop(0)
+            merged_arr[i] = arrB[b]
+            b += 1
 
     return merged_arr
 
@@ -35,5 +40,15 @@ def merge_in_place(arr, start, mid, end):
     pass
 
 
-def merge_sort_in_place(arr, l, r):
-    pass
+def merge_sort_in_place(arr, l=0, r=None):
+    if not r:
+        r = len(arr) - 1
+    if r <= l:
+        return
+    mid = (r+l+1)//2
+    merge_sort_in_place(arr, l, mid-1)
+    merge_sort_in_place(arr, mid, r)
+    return merge_in_place(arr, l, mid, r)
+
+# arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# merge_sort_in_place(arr1)
