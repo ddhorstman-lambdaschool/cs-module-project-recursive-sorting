f02d2ab19abea406dd276ce2760b63ac843bad87
David Horstman <davidzilla27@gmail.com>
2020-07-22 19:23:52 -0400

    Return to working version

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -30,14 +30,11 @@ def agnostic_binary_search(arr, target, start=0, end=None):
         return mid
     if ascending:
         if arr[mid] < target:
-            return agnostic_binary_search(arr, target, start=mid+1)
+            return agnostic_binary_search(arr, target, mid+1, end)
         else:
-            return agnostic_binary_search(arr, target, end=mid-1)
+            return agnostic_binary_search(arr, target, start, mid-1)
     else:
         if arr[mid] < target:
-            return agnostic_binary_search(arr, target, end=mid-1)
+            return agnostic_binary_search(arr, target, start, mid-1)
         else:
-            return agnostic_binary_search(arr, target, start=mid+1)
-
-ascending = [2, 4, 12, 14, 17, 30, 46, 47, 51, 54, 61]
-agnostic_binary_search(ascending, 31)
+            return agnostic_binary_search(arr, target, mid+1, end)
